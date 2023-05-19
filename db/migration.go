60a8d5e71d4ca13f25b4ae1385a0f4300de94b8c
krishpranav <krisna.pranav@gmail.com>
2023-05-19 17:29:33 +0530

    migration + sqlite[database]

--- db/migration.go
+++ db/migration.go
@@ -9,10 +9,22 @@
 package db
 
 const CreateMigrationTable string = `
-		CREATE TABLE IF NOT EXISTS migration (
-			name TEXT NOT NULL PRIMARY KEY,
-			created_at INTEGER NOT NULL
-		);
+	CREATE TABLE IF NOT EXISTS migration (
+		name TEXT NOT NULL PRIMARY KEY,
+		created_at INTEGER NOT NULL
+	);
+`
+
+const NewMigrationEntry string = `
+	INSERT INTO migration (name, created_at) VALUES (?, ?)
+`
+
+const FindMigrationEntry string = `
+	SELECT name FROM migration WHERE name = ?
+`
+
+const DeleteMigrationEntry string = `
+	DELETE FROM migration WHERE name = ?
 `
 
 type SQLMigration struct {
---
+++ db/sqlite.go
@@ -0,0 +1,58 @@
+//
+//
+// @filename: db/sqlite.go
+// @author: Krisna Pranav
+// @license COPYRIGHT 2023 Krisna Pranav, NanoBlocksDevelopers
+//
+//
+
+package db
+
+import (
+	"database/sql"
+	"time"
+)
+
+type SQLite struct {
+	Db         *sql.DB
+	migrations []SQLMigration
+}
+
+func (s *SQLite) Migrations() []SQLMigration {
+	return s.migrations
+}
+
+func (s *SQLite) AddMigration(migration ...SQLMigration) {
+	s.migrations = append(s.migrations, migration...)
+}
+
+func (s *SQLite) RunMigrations() error {
+	if len(s.migrations) < 1 {
+		return nil
+	}
+
+	for _, migration := range s.migrations {
+		row := s.Db.QueryRow(FindMigrationEntry, migration.name)
+		if row.Err() != nil {
+			return row.Err()
+		}
+
+		var name string
+		_ = row.Scan(&name)
+		if name != "" {
+			continue
+		}
+
+		if _, err := s.Db.Exec(migration.upQuery); err != nil {
+			return err
+		}
+
+		_, err := s.Db.Exec(NewMigrationEntry, migration.name, time.Now().Unix())
+		if err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+}
