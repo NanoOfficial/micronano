bdff08bf94ee6b88626c57b8678cfd767f4954b5
krishpranav <krisna.pranav@gmail.com>
2023-05-19 14:13:38 +0530

    common: bytes

---
+++ common/bytes.go
@@ -0,0 +1,17 @@
+//
+//
+// @filename: common/bytes.go
+// @author: Krisna Pranav
+// @license COPYRIGHT 2023 Krisna Pranav, NanoBlocksDevelopers
+//
+//
+
+package common
+
+type StructBytes struct {
+}
+
+func (sb *StructBytes) Bytes() []byte {
+	bytes := []byte{}
+	return bytes
+}
